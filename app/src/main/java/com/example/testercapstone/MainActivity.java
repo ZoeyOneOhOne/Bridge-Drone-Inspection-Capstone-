298dc2517ebdd1cbd963c1c1fceffbdde185bfda
c-wallace <wallacec733@gmail.com>
2019-04-01 17:53:10 -0400

    droneMeta library added

--- app/src/main/java/com/example/testercapstone/MainActivity.java
+++ app/src/main/java/com/example/testercapstone/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testercapstone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/testercapstone/descriptionGallery.java
+++ app/src/main/java/com/example/testercapstone/descriptionGallery.java
@@ -3,11 +3,17 @@ package com.example.testercapstone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+
 public class descriptionGallery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description_gallery);
+        //every new file path you will make a dronemeta object for that file
+        File test = new File("test.jpg");
+        DroneMeta meta = new DroneMeta(test);
+
     }
 }
