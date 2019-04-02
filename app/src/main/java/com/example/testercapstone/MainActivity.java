46bdb032dbe777745b69f0f176098aac7e8a97aa
Cole Perry <perry391@marshall.edu>
2019-04-02 02:06:52 -0700

    Started on pop up window
    I added a button to the login page just to test the pop up functionality. This button will be removed later it is simply for testing.

--- app/src/main/java/com/example/testercapstone/MainActivity.java
+++ app/src/main/java/com/example/testercapstone/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.testercapstone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import java.io.File;
 
+//The pop up window code was inspired by a Youtuber of the name Angga Risky: https://www.youtube.com/watch?v=eX-TdY6bLdg
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_Cam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_Cam = (Button) findViewById(R.id.btn_Cam);
+
+        btn_Cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PopupActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/testercapstone/PopupActivity.java
@@ -0,0 +1,37 @@
+package com.example.testercapstone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.content.Intent;
+import android.util.DisplayMetrics;
+//NOTE: Alt + Enter will auto import the one you need.
+
+public class PopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic_gallery);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8), (int) (height*.7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+
+
+    }
+}
