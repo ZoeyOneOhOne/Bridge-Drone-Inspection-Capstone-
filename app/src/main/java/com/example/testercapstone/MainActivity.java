1c6988ca7173b754d2d90a9244a2e468a7d50cfd
c-wallace <wallacec733@gmail.com>
2019-04-20 13:02:44 -0400

    FileObserver

--- app/src/main/java/com/example/testercapstone/MainActivity.java
+++ app/src/main/java/com/example/testercapstone/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.testercapstone;
 
 import android.content.Intent;
+import android.os.FileObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +13,7 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     Button loginbtn, toWritePage;
+    private static final String TAG = "mainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        //this is to try and listen to the file and then pull up the opop up activity wiht the intent
+        FileObserver observer = new FileObserver(android.os.Environment.getExternalStorageDirectory().toString() + "/DJI/dji.go.v4/CACHE_IMAGE") { // set up a file observer to watch this directory on sd card
+            @Override
+            public void onEvent(int event, String file) {
+                if(event == FileObserver.CREATE){ // check if its a "create"
+                    Log.d(TAG, "File created [" + android.os.Environment.getExternalStorageDirectory().toString() + "/DCIM/100MEDIA/" + file + "]");
+                    Intent i = new Intent(getApplicationContext(), PopupActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
 
 
         loginbtn = (Button) findViewById(R.id.loginbtn);
